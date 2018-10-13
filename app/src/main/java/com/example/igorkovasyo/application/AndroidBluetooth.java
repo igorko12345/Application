cb5d65eea862f5009f2df4a6873c0cd83085ebc3
igorko12345 <igor.vasyo.199988@gmail.com>
2018-10-13 21:35:04 +0300

    Add list paired devices (bluetooth)

--- app/src/main/java/com/example/igorkovasyo/application/AndroidBluetooth.java
+++ app/src/main/java/com/example/igorkovasyo/application/AndroidBluetooth.java
@@ -6,17 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class AndroidBluetooth extends AppCompatActivity {
+    private static final int REQUEST_PAIRED_DEVICE = 2;
+
+    Button btnListPairedDevices;
+    TextView stateBluetooth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btntOn = (Button)findViewById(R.id.btnOn);
-        Button btntOff = (Button)findViewById(R.id.btnOFF);
-        Button btnDisc = (Button)findViewById(R.id.btnDiscoverable);
+        setContentView(R.layout.main);
+
+        Button btntOn = findViewById(R.id.btnOn);
+        Button btntOff = findViewById(R.id.btnOFF);
+        Button btnDisc = findViewById(R.id.btnDiscoverable);
+        btnListPairedDevices = findViewById(R.id.listpaireddevices);
+
+        stateBluetooth = findViewById(R.id.bluetoothstate);
         final BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+        btnListPairedDevices.setOnClickListener(btnListPairedDevicesOnClickListener);
         btntOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,4 +59,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private Button.OnClickListener btnListPairedDevicesOnClickListener
+            = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View arg0) {
+            Intent intent = new Intent();
+            intent.setClass(AndroidBluetooth.this, ListPairedDevicesActivity.class);
+            startActivityForResult(intent, REQUEST_PAIRED_DEVICE);
+        }};
 }
